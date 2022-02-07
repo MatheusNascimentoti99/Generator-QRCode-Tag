3342c11aac12dbeef96672ba3f571f6ca52a031f
Matheus Nascimento <matheusnascimentoti99@gmail.com>
2022-02-07 11:20:40 -0300

    Up

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <canvas id="canvas"></canvas>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  export default {
+    name: 'App',
+    components: {},
+    mounted() {
+      var QRCode = require('qrcode');
+      var canvas = document.getElementById('canvas');
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+      QRCode.toCanvas(canvas, 'sample text', function (error) {
+        if (error) console.error(error);
+        console.log('success!');
+      });
+    },
+  };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
