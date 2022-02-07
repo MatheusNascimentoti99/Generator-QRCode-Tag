c221db5b51b405a70e01ff1b769a28743a4b3ee4
Matheus Nascimento <matheusnascimentoti99@gmail.com>
2022-02-07 11:53:38 -0300

    :hammer: Generate qr_code

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,31 @@
 <template>
-  <canvas id="canvas"></canvas>
+  <div class="container">
+    <div class="box" v-for="value in qtdBoxes" :key="value">
+      <canvas :id="'canvas'+value" />
+      <h1 class>Caixa: {{value}}</h1>
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
     name: 'App',
+    data() {
+      return {
+        qtdBoxes: 300,
+      };
+    },
     components: {},
     mounted() {
       var QRCode = require('qrcode');
-      var canvas = document.getElementById('canvas');
-
-      QRCode.toCanvas(canvas, 'sample text', function (error) {
-        if (error) console.error(error);
-        console.log('success!');
-      });
+      for (let i = 1; i <= this.qtdBoxes; i++) {
+        let canvas = document.getElementById('canvas' + i);
+        console.log(canvas);
+        QRCode.toCanvas(canvas, i + '', { scale: 7 }, function (error) {
+          if (error) console.error(error);
+          console.log('success!');
+        });
+      }
     },
   };
 </script>
@@ -26,5 +38,28 @@
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
+  }
+
+  .container {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    flex-direction: row;
+  }
+  .box {
+    display: flex;
+    width: 22cm;
+    height: 6cm;
+    justify-content: space-evenly;
+    flex-direction: row;
+    align-items: center;
+    border: 2px solid black;
+    margin: 2px;
+    padding: 0px 8px 0px 8px;
+  }
+
+  .text-box {
   }
 </style>
